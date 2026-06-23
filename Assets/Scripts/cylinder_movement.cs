2bfc6e88a69e731b07d316a8f4a27892caa93ec8
Amol <amol100gupta@gmail.com>
2026-06-24 01:20:23 +0530

    Movement complete
    - movement tested and working
    - both w,a,s,d and arrow keys  work
    - collision has not been added yet
    - movement restricted in XZ plane
    - jump not added yet

--- Assets/Scripts/cylinder_movement.cs
+++ Assets/Scripts/cylinder_movement.cs
@@ -3,12 +3,12 @@ using UnityEngine.InputSystem;
 
 public class cylinder_movement : MonoBehaviour
 {
-    Vector3 motion_dir = new Vector3(0f, 0f, 0f);
+    Vector3 motion_dir = new Vector3(11.0f, 0.0f, -1341.0f);
     Vector3 cam_dir = new Vector3(1f, 0f, 1f);
-    float speed = 20.0f;
+    float speed = 5.0f;
     public Transform targetCameraTransform;
-    float verticalInput = 0.0f;
-    float horizontalInput = 0.0f;
+    int verticalInput = 0;
+    int horizontalInput = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,14 +22,24 @@ public class cylinder_movement : MonoBehaviour
         cam_dir.x = 0 ;
         if (Keyboard.current != null)
         {
-            if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed) verticalInput = 1f;
-            if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed) verticalInput = -1f;
-            if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed) horizontalInput = 1f;
-            if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed) horizontalInput = -1f;
+            if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed) verticalInput = 1;
+            if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed) verticalInput = -1;
+            if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed) horizontalInput = 1;
+            if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed) horizontalInput = -1;
+        
+            if (horizontalInput != 0)
+            {
+                motion_dir.x = transform.position.x + speed*horizontalInput;
+                transform.position = motion_dir;
+                horizontalInput = 0;
+            }
+            if (verticalInput != 0)
+            {
+                motion_dir.z = transform.position.z + speed*verticalInput;
+                transform.position = motion_dir;
+                verticalInput = 0;
+            }
         }
-        if (horizontalInput == 1)
-            motion_dir.x = transform.position.x + speed;
-        horizontalInput = 0;
-        verticalInput = 0;
+        
     }
 }
