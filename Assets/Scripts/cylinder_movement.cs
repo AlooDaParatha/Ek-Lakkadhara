5db88fed62499da63bb55c057b5b253a20c2a491
Amol <amol100gupta@gmail.com>
2026-06-25 00:59:04 +0530

    added camera rotation
    - added rotation of camera using mouse
    - movement is not yet aligned with rotation

--- Assets/Scripts/cylinder_movement.cs
+++ Assets/Scripts/cylinder_movement.cs
@@ -9,6 +9,9 @@ public class cylinder_movement : MonoBehaviour
     public Transform targetCameraTransform;
     int verticalInput = 0;
     int horizontalInput = 0;
+    public cam_rotation cam_turn;
+
+    public Vector2 turn;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,28 +21,31 @@ public class cylinder_movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        cam_dir = targetCameraTransform.forward;
-        cam_dir.x = 0 ;
+        turn.x += Input.GetAxis("Mouse X");
+        turn.y += Input.GetAxis("Mouse Y");
+        transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0f);
+        
         if (Keyboard.current != null)
         {
             if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed) verticalInput = 1;
             if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed) verticalInput = -1;
             if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed) horizontalInput = 1;
             if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed) horizontalInput = -1;
-        
+            
             if (horizontalInput != 0)
             {
-                motion_dir.x = transform.position.x + speed*horizontalInput;
+                motion_dir.x = transform.position.x + speed*Mathf.Cos(turn.y * 3.14f / 90.0f) * horizontalInput;
+                motion_dir.z = transform.position.z + speed*Mathf.Sin(turn.y * 3.14f / 90.0f) * horizontalInput;
                 transform.position = motion_dir;
                 horizontalInput = 0;
             }
             if (verticalInput != 0)
             {
-                motion_dir.z = transform.position.z + speed*verticalInput;
+                motion_dir.x = transform.position.x + speed*Mathf.Cos(turn.y * 3.14f / 90.0f) * verticalInput;
+                motion_dir.z = transform.position.z + speed*Mathf.Sin(turn.y * 3.14f / 90.0f) * verticalInput;
                 transform.position = motion_dir;
                 verticalInput = 0;
             }
         }
-        
     }
 }
